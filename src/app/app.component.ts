201815f78ee9fb66ced71203f1dd9c58fcbe86e4
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2020-12-07 23:54:07 +0530

    update index html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //component name , tag name
   selector: 'app-root',
+
+  //html file location of the component
   templateUrl: './app.component.html',
+
+  //styling file location of the component
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'todo';
+  testMessage = 'test message' //interpollation
+
+
 }
