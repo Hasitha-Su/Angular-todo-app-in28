d6ac239e826d00d5426ae561f3ff4189a9da16e7
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2020-12-08 20:45:48 +0530

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // defining an object
+  { path:'', component: LoginComponent },
+  { path:'login', component: LoginComponent },
+  { path:'welcome', component: WelcomeComponent},
+
+  //Anything else defined
+  { path:'**', component: ErrorComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
