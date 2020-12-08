530abafe8774db2d307ab01c7261d8b1a47d38cb
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2020-12-08 23:32:27 +0530

    add todo-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
 
   // defining an object
-  { path:'', component: LoginComponent },
-  { path:'login', component: LoginComponent },
-
+  { path:'', component:LoginComponent },
+  { path:'login', component:LoginComponent },
+  { path:'todos', component:ListTodosComponent},
   
   // { path:'welcome', component: WelcomeComponent},
   // router with parameters
