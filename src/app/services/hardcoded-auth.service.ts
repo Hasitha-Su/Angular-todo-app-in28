f32b06a07d97e1a29b5498c326cc777745f6306a
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2020-12-15 23:59:44 +0530

    add auth services

---
+++ src/app/services/hardcoded-auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthService {
+
+  constructor() { }
+
+  //implement method to user authenticate
+  authenticate(username, password){
+
+    if(password === 'pass'){
+      //console.log('before '+ this.isUserLoggedIn() )
+      sessionStorage.setItem('authenticatedUser', username)
+      //console.log('after '+ this.isUserLoggedIn() )
+      return true
+    }else{
+      return false
+    }
+
+  }
+
+  //check user auth
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    return !(user === null)
+  }
+
+
+}
